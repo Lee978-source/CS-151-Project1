aae55209ccc08b1f92b378e61e6218b9c4bc4fdb
Brian Nguyen <brian.cali230@gmail.com Primary Verified>
2025-09-25 20:29:38 -0700

    Updating DocPDF with additional methods and implemented the interface exportable class

--- DocPDF.java
+++ DocPDF.java
@@ -4,16 +4,25 @@
  * CS151 Fall 2025 - Project 1
  */
 
-public class DocPDF extends GenericPDF  {
-protected String text;
+public class DocPDF extends GenericPDF implements Exportable   {
+private String textContent;
 
     public DocPDF(String username, String email, String role, String text) {
         super(username, email, role);
-        this.text = text;
+        this.textContent = text;
     }
 
+    public String getText() {
+        return textContent;
+    }
+
+    public void setText(String text) {
+        this.textContent = text;
+    }
+
+
     public void addPageBreaker() {
-        System.out.println("--------------------");
+        System.out.println("-------------------------");
     }
 
     public void deleteLatestPage() {
@@ -28,14 +37,57 @@ protected String text;
 
     }
 
+    public int getPageCount() {
+
+    }
+
+    public void addPageNumber() {
+
+    }
+
+    public void exportAsPDF() {
+
+    }
+
+    public void exportAsHTML() {
+
+    }
+
+    public void exportAsWordDoc() {
+
+    }
+
+
+    public boolean findWord(String word) {
+        if(textContent != null && textContent.contains(word)) {
+            System.out.println("Word found: " + word);
+            return true;
+        }
+        else {
+            System.out.println("Word was not found");
+            return false;
+        }
+    }
+
+    @Override
+    public void merge() {
+
+    }
+
+    @Override
+    public void split() {
+
+    }
+
     @Override
     public String toString() {
         return "GenericPDF{" +
                 "username='" + username + '\'' +
                 ", email='" + email + '\'' +
-                ", PDF Text='" + role + '\'' +
-                ", Word Count= " + getWordCount() +
-                ", Character Count=" + getCharCount() +
+                ", PDF Text='" + textContent + '\'' +
+                ", Word Count= " + getWordCount() + '\'' +
+                ", Character Count=" + getCharCount() + '\'' +
+                ", Page Count=" + getPageCount() + '\'' +
                 '}';
     }
 }
---
+++ Exportable.java
@@ -0,0 +1,14 @@
+/**
+ * @author [Brian Nguyen]
+ * @version 1.0
+ * CS151 Fall 2025 - Project 1
+ */
+
+// interface class
+interface Exportable {
+    // methods to export into a different format
+    void exportAsPDF();
+    void exportAsHTML();
+    void exportAsWordDoc();
+}
+
