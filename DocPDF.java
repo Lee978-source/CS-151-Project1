cfb020df52c165e3e92a3df6390f6385ee63c3e7
Brian Nguyen <brian.cali230@gmail.com Primary Verified>
2025-09-26 17:18:58 -0700

    Updating DocPDF with additional methods and implemented the interface exportable class

--- DocPDF.java
+++ DocPDF.java
@@ -4,12 +4,22 @@
  * CS151 Fall 2025 - Project 1
  */
 
-public class DocPDF extends GenericPDF implements Exportable   {
-private String textContent;
+import java.util.ArrayList;
+import java.util.List;
 
-    public DocPDF(String username, String email, String role, String text) {
+public class DocPDF extends GenericPDF implements Exportable   {
+    private String textContent;
+    private List<String> pages = new ArrayList<>();
+
+
+    public DocPDF(String username, String email, String role, String textContent) {
         super(username, email, role);
-        this.textContent = text;
+
+        if (textContent == null) {
+            pages.add("");
+        } else {
+            pages.add(textContent);
+        }
     }
 
     public String getText() {
@@ -20,43 +30,49 @@ private String textContent;
         this.textContent = text;
     }
 
+    public void addTextToCurrPage(String text) {
+        int last = pages.size() - 1;
+        pages.set(last, pages.get(last) + " " + text);
+    }
 
     public void addPageBreaker() {
-        System.out.println("-------------------------");
+        pages.add("");
+        System.out.println("Page " + pages.size() + " created.");
     }
 
     public void deleteLatestPage() {
-
+        if (!pages.isEmpty()) {
+            int last = pages.size() - 1;
+            pages.remove(last);
+        }
+        else {
+            System.out.println("No pages to delete");
+        }
     }
 
     public int getWordCount() {
-
+        int total = 0;
+        for(String page : pages) {
+            if(page != null && !page.trim().isEmpty()) {                // checks if the page is not there and makes sure after trimming
+                total += page.trim().split("\\s+").length;        // that the page is not empty
+            }
+        }
+        return total;   // returns the total word count in the page
     }
 
     public int getCharCount() {
-
+        int total = 0;
+        for(String page : pages) {
+            if(page != null && !page.trim().isEmpty()) {
+                total += page.trim().length();
+            }
+    }
+        return total;
     }
 
     public int getPageCount() {
-
+        return pages.size();
     }
-
-    public void addPageNumber() {
-
-    }
-
-    public void exportAsPDF() {
-
-    }
-
-    public void exportAsHTML() {
-
-    }
-
-    public void exportAsWordDoc() {
-
-    }
-
 
     public boolean findWord(String word) {
         if(textContent != null && textContent.contains(word)) {
@@ -71,13 +87,29 @@ private String textContent;
 
     @Override
     public void merge() {
-
+        System.out.println("Merging doc with another doc");
     }
 
     @Override
     public void split() {
-
+        System.out.println("Splitting doc into separate doc");
     }
+
+    @Override
+    public void exportAsPDF() {
+        System.out.println("Exporting slides as PDF");
+    }
+
+    @Override
+    public void exportAsHTML() {
+        System.out.println("Exporting slides as HTML");
+    }
+
+    @Override
+    public void exportAsWordDoc() {
+        System.out.println("Exporting slides as Word Document");
+    }
+
 
     @Override
     public String toString() {
