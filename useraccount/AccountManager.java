5b1fa7f688975e07bc0098169053ca3487464ba5
Ethan Le <enathanle@gmail.com>
2025-10-04 19:22:26 -0700

    Added getter and setter methods for private variables, finalized option selections for user and their corresponding methods and logic.

--- useraccount/AccountManager.java
+++ useraccount/AccountManager.java
@@ -10,7 +10,7 @@
  public class AccountManager {
 
     /** General ArrayList to hold user accounts: */
-    private ArrayList<AccountManager> accounts = new ArrayList<>(); // Overall ArrayList that holds all existing user accounts. 
+    private static ArrayList<AccountManager> accounts = new ArrayList<>(); // Overall ArrayList that holds all existing user accounts (only one to belong to WHOLE AccountManager class). 
 
     /** User Information Fields: */
     private String username; // First and last name. 
@@ -25,33 +25,52 @@
     /** Constructor to initialize User Account object: */
     public AccountManager(String username, String email, String password, String dateOfBirth) {
         
-        // Initialize Account Information: 
-        this.username = username; 
-        this.email = email; 
-        this.password = password; 
-        this.dateOfBirth = dateOfBirth;
+    	if (!email.contains("@gmail.com")) // Ensure that the email has "@gmail.com" in it. 
+    	{
+    		System.out.println("Account creation failed! Email must contain \"@gmail.com\"!"); 
+    	}
+    	
+    	else if (password.strip().length() < 8) // Ensure that the password is 8 characters long, excluding any white spaces. 
+    	{
+    		System.out.println("Account creation failed! Password must be at least 8 characters long!"); 
+    	}
+    	
+    	else 
+    	{
+    		// Initialize Account Information: 
+            this.username = username; 
+            this.email = email; 
+            this.password = password; 
+            this.dateOfBirth = dateOfBirth;
 
-        // Initalize Account Attributes: 
-        this.inbox = new ArrayList<>(); // Initialize empty inbox for the new user account. 
-        this.drive = new Drive(); // Initialize empty Drive for the new user account. 
+            // Initalize Account Attributes: 
+            this.inbox = new ArrayList<>(); // Initialize empty inbox for the new user account. 
+            this.drive = new Drive(); // Initialize empty Drive for the new user account. 
 
-        // Add the new account object to the General ArrayList of accounts: 
-        accounts.add(this); // Add this instance (object) of AccountManager to the "accounts" ArrayList. 
+            // Add the new account object to the General ArrayList of accounts: 
+            accounts.add(this); // Add this instance (object) of AccountManager to the "accounts" ArrayList.
+            
+            System.out.println("Account creation successful! Welcome to your new account!"); 
+    	}
     }
 
     /** Method to validate user login: */
-    public void login(String email, String password) {
+    public static AccountManager login(String email, String password) {
 
         // Method to handle user login (match the email and password). 
-        for (AccountManager account : accounts) // Loop through each existing account in the general "accounts" ArrayList until we find a match.
+        for (AccountManager account : getAccountsDatabase()) // Loop through each existing account in the general "accounts" ArrayList until we find a match.
         {
-            if (account.email.equals(email) && account.password.equals(password)) // If the email and password match, then the user can log in. 
+            if (account.getEmail().equals(email) && account.getPassword().equals(password)) // If the email and password match, then the user can log in. 
             {
                 System.out.println("User successfully logged in! Welcome, " + account.username + "!"); // Give welcome message to user. 
 
-                accountMenu(); // Then call the account menu method to print menu selection of options that the user can do. 
+                account.accountMenu(); // Then call the account menu method to print menu selection of options that the user can do. 
+                
+                return account; 
             }
         }
+        
+        return null; 
     }
 
     /** Method to print menu options for user to select: */
@@ -63,7 +82,7 @@
         System.out.println("Drive Menu Options: \n");
         System.out.println("(1) Create Document"); 
         System.out.println("(2) Create Slides"); 
-        System.out.println("(3) Create Slides.Spreadsheet");
+        System.out.println("(3) Create Spreadsheet");
         System.out.println("(4) Edit PDF");  
         System.out.println("(5) View Drive File Names"); 
         System.out.println("(6) View Drive Storage\n" + "-------------------------------"); 
@@ -74,7 +93,168 @@
         System.out.println("(8) Send Email"); 
         System.out.println("(9) Get Account Info"); 
         System.out.println("(10) Change password"); 
-        System.out.println("(11) Logout"); 
+        System.out.println("(11) Change username");
+        System.out.println("(12) Change email");
+        System.out.println("(13) Change date of birth");
+        System.out.println("(14) Logout"); 
+    }
+    
+    /** Getter methods for each private property of the user account: */
+    public String getUsername()
+    {
+    	return this.username; 
+    }
+    
+    public String getEmail()
+    {
+    	return this.email; 
+    }
+    
+    public String getPassword()
+    {
+    	return this.password; 
+    }
+    
+    public String getDateOfBirth()
+    {
+    	return this.dateOfBirth; 
+    }
+    
+    public ArrayList<String> getInbox()
+    {
+    	return this.inbox; 
+    }
+    
+    public Drive getDrive()
+    {
+    	return this.drive; 
+    }
+    
+    public static ArrayList<AccountManager> getAccountsDatabase()
+    {
+    	return accounts; // Static variable of the whole class, so there is no "this." instance. 
     }
 
+    /** Method to take user option for creating or editing PDF: */
+    public void createOption(int option, String fileName) // In "Main" class, user will be prompted to enter the File Name if they have selected Options 1-4 ("Main" class would call this method). 
+    {
+    	switch (option) {
+    		case 1, 2, 3:
+    			getDrive().createPDF(option, fileName, getUsername(), getEmail()); // Create a new PDF based on user option, their scanned file name, and their username and email.
+    			break; 
+    		case 4:
+    			getDrive().editPDF(fileName); // Use the file name to locate the appropriate PDF to view and edit. 
+    			break; 
+    		default: 
+    			break; 
+    	}
+    }
+    
+    /** Method to take user option for viewing Drive contents: */
+    public void viewDriveOption(int option) // Call this method in the "Main" class if the user had selected Options 5-6. 
+    {
+    	switch (option) {
+		    case 5: 
+				getDrive().viewAllFileTitles(); // View all of the existing files in the Drive. 
+				break; 
+			case 6: 
+				getDrive().checkStorage(); // Check used Drive storage and remaining Drive storage. 
+				break; 
+    	}
+    }
+    
+    /** Method to return number of emails in inbox (another Getter method): */
+    public int getNumberOfEmails() // Call this method FIRST in the "Main" class if the user had selected Option 7. 
+    {
+    	return getInbox().size(); // Return the number of emails to the user. The "Main" class will use this number to print a message to the user either asking them to enter an email number to read (if inbox size > 0), or a message that just tells them they have no emails to read (else inbox size == 0). 
+    }
+    
+    /**
+    // Method to show number of emails in inbox and to call another method to read a specific email: 
+    public void getNumberOfEmails() // If the user had entered Option 7, the "Main" class will call this method first, then it will call "fetchEmail()" in the "Main" class once the user enters an email number. 
+    {
+    	if (this.inbox.size() > 0) // Condition if user has at least one email. 
+    		System.out.println("You have " + this.inbox.size() + " emails! Read which email? (Enter any number from 1 to " + this.inbox.size() + "): "); // Tell user how many emails they currently have, and ask them for a number for what email to read. 
+    	else // Condition if user has no emails. 
+    		System.out.println("You have " + this.inbox.size() + " emails! No emails to read!"); // Tell user they have no emails. 
+    }
+    **/
+    
+    /** Method to read specified email: */ 
+    public void fetchEmail(int emailNumber) // Call this method NEXT in the "Main" class (from Option 7) once the user has entered an email number to read. 
+    {
+    	System.out.println(emailNumber + ": " + getInbox().get(emailNumber-1)); // Print the message number and the message itself. 
+    }
+    
+	/** Method to send an email: */ 
+    public void sendEmail(String recipient, String message) // In "Main" class, user will be prompted to enter recipient email and the message before calling this method (if they have chosen Option 8). 
+    {
+    	boolean sent = false; 
+    	
+    	for (AccountManager account : getAccountsDatabase()) // Loop through all existing accounts in the database to ensure the recipient email exists. 
+    	{
+    		if (account.getEmail().equals(recipient)) // If the recipient email exists, 
+    		{
+    			account.getInbox().add(message); // then send the message to their inbox. 
+    			System.out.println("Email sent to " + account.getUsername() + " successfully!"); 
+    			sent = true; // Mark the "sent" flag to true. 
+    			break; 
+    		}
+    	}
+    	
+    	if (sent == false) // Use the "sent" flag to send user error message if user did not enter a valid recipient email. 
+    	{
+    		System.out.println("User does not exist! Please enter a valid recipient email!"); 
+    	}
+    }
+    
+    /** Method to get the user's account information: */ 
+    @Override
+    public String toString() // In "Main" class, call this method if user has selected Option 9. 
+    {
+    	return "Username: " + getUsername() + "\nEmail: " + getEmail() + "\nPassword: " + getPassword() + "\nDate of Birth: " + getDateOfBirth() + "\nNumber of Emails in Inbox: " + getNumberOfEmails(); 
+    }
+	
+    /** Setter methods for each private property of the user account: */
+    
+    /** Method to change the user's password: */
+    public void setPassword(String newPassword) // In "Main" class, if user has selected Option 10, have them first enter their old password, then call "getPassword()" from this class to get the old password to see if it matches, then have user enter NEW password and call this method to change it. 
+    {
+    	if (newPassword.strip().length() >= 8) // Ensure that the password is 8 characters long, excluding any white spaces.
+    	{
+    		this.password = newPassword; 
+    		System.out.println("Password change successful!"); 
+    	}
+    	else
+    		System.out.println("Password change failed! Password must be at least 8 characters long!"); 
+    }
+    
+    /** Method to change the user's username: */
+    public void setUsername(String newUsername) // In "Main" class, if user has selected Option 11, prompt them to enter new username. 
+    {
+    	this.username = newUsername; 
+    	System.out.println("Username change successful!"); 
+    }
+    
+    /** Method to change the user's email: */
+    public void setEmail(String newEmail) // In "Main" class, if user has selected Option 12, prompt them to enter new email. 
+    {
+    	if (newEmail.contains("@gmail.com")) // Ensure that the email has "@gmail.com" in it. 
+    	{
+    		this.email = newEmail; 
+    		System.out.println("Email change successful!"); 
+    	}
+    	
+    	else
+    	{
+    		System.out.println("Email change failed! Email must contain \"@gmail.com\"!"); 
+    	}
+    }
+    
+    /** Method to change the user's date of birth: */
+    public void setDateOfBirth(String newDateOfBirth) // In "Main" class, if user has selected Option 13, prompt them to enter new date of birth. 
+    {
+    	this.dateOfBirth = newDateOfBirth; 
+    	System.out.println("Date of Birth change successful!"); 
+    }
  }
