043fc5adf5e034da8a45f2d1913012d90306ab23
EthanLe-hub <enathanle@gmail.com>
2025-10-07 14:26:15 -0700

    Update Spreadsheet.java
    Overridden merge() and split() methods to implement proper functionality with Spreadsheet.java.

--- PDF/Spreadsheet.java
+++ PDF/Spreadsheet.java
@@ -4,10 +4,12 @@
      * CS151 Fall 2025 - Project 1
      */
     package PDF;
-     
-    public class Spreadsheet extends GenericPDF {
-        private final int rows, cols;
-        private final String[][] matrix;
+
+import java.util.ArrayList;
+
+public class Spreadsheet extends GenericPDF {
+        private int rows, cols;
+        private String[][] matrix;
 
         public Spreadsheet(String username, String email, String role) {
             super(username, email, role);
@@ -39,8 +41,85 @@
 
         private boolean in(int r, int c) { return r>=0 && r<rows && c>=0 && c<cols; }
 
-        @Override public void merge(GenericPDF file1, GenericPDF file2)            { System.out.println("Merging spreadsheets"); }
-        @Override public GenericPDF split(int splitIndex)            { System.out.println("Splitting spreadsheet"); return null;}
+        @Override
+    public void merge(GenericPDF otherSheet) {
+        if (otherSheet == null) {
+            System.out.println("Cannot merge with null Sheet :(");
+            return;
+        }
+        if (!this.getRole().equals("OWNER") && !this.getRole().equals("EDITOR")) {
+            System.out.println("You do not have permission to merge slides.");
+            return;
+        }
+        int originalRow = this.rows;
+        int originalCol = this.cols; 
+
+        this.rows += ((Spreadsheet) otherSheet).rows;
+        this.cols += ((Spreadsheet) otherSheet).cols;
+
+        String[][] newMatrix = new String[this.rows][this.cols]; // New matrix with new size. 
+
+        for (int row = 0; row < originalRow; row++) // Old content from OG sheet has the same indices. 
+        {
+            for (int col = 0; col < originalCol; col++)
+            {
+                newMatrix[row][col] = this.matrix[row][col]; 
+            }
+        }
+
+        for (int row = 0; row < this.rows; row++) // New content from other sheet shifted by original size.
+        {
+            for (int col = 0; col < this.cols; col++)
+            {
+                newMatrix[this.rows+row][this.cols+col] = this.matrix[row][col]; 
+            }
+        }
+
+        this.matrix = newMatrix; // Update reference to the NEW matrix. 
+
+        System.out.println("Original sheet row count: " + originalRow + "\nOriginal sheet col count: " + originalCol);
+        System.out.println("Merged sheet row count: " + this.rows + "\nMerged sheet col count: " + this.cols);
+        System.out.println("Merged sheets. Total cells: " + this.rows * this.cols);
+    }
+
+    @Override
+    public Spreadsheet split(int splitIndex) {
+        if (splitIndex <= 0 || splitIndex >= this.cols) {
+            System.out.println("Invalid split index at column." + (this.cols - 1));
+            return null;
+        }
+        
+         String[][] newMatrix = new String[this.rows][this.cols - splitIndex];
+         String[][] oldMatrix = new String[this.rows][splitIndex];
+
+         for (int row = 0; row < this.rows; row++) // Split content into two matrices. 
+         {
+             for (int col = 0; col < this.cols; col++)
+             {
+                 if (col < splitIndex) // If we are still in the left "half" of the Spreadsheet, keep the old content in the same indices.
+                     oldMatrix[row][col] = this.matrix[row][col]; 
+                 else // otherwise, if we are still in the right "half" of the Spreadsheet, place the remaining content in left-shifted indices. 
+                     newMatrix[row][col - splitIndex] = this.matrix[row][col]; 
+             }
+         }
+
+         this.matrix = oldMatrix; // Update reference to the OLD matrix (left half of the content)
+
+         System.out.println("Original sheet row count: " + this.rows + "\nMerged sheet col count: " + this.cols);
+         
+         this.rows = oldMatrix.length; // Left sheet row count. 
+         this.cols = oldMatrix[0].length; // Left sheet col count. 
+
+         System.out.println("Left sheet row count after split: " + this.rows + "\nLeft sheet col count after split: " + this.cols);
+         System.out.println("Right sheet row count after split: " + newMatrix.length + "\nRight sheet col count after split: " + newMatrix[0].length);
+         
+         System.out.println("Spreadsheet split at column " + splitIndex);
+         
+         Spreadsheet newSheet = new Spreadsheet(this.username, this.email, this.role);
+         newSheet.matrix = newMatrix; // Right half of the content. 
+
+         return newSheet;
+    }
         @Override public void exportAsPDF()      { System.out.println("Exporting sheet as PDF"); }
         //@Override public void exportAsHTML()     { System.out.println("Ex";}
 
